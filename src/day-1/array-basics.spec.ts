a10ec4de60341507a2b59ff1f8336046a203b70a
aliyurekli32 <adamjesperblack@gmail.com>
2022-08-30 08:20:00 -0500

    add test for zero for removeduplicates

---
+++ src/day-1/array-basics.spec.ts
@@ -0,0 +1,28 @@
+//26. Remove Duplicates from Sorted Array
+//write test cases using zombie( Z – Zero, O – One, M – Many (or More complex), B – Boundary Behaviors, I – Interface definition , E – Exercise Exceptional behavior, S – Simple Scenarios, Simple Solutions) tdd technique )
+
+const removeDuplicates = (nums: number[]): number => {
+    if (nums.length === 0) return 0;
+    return 0
+};
+describe('removeDuplicates', () => {
+    it('should remove duplicates from a sorted zore array', () => {
+      const nums: number[] = [];
+      const expected = 0;
+      const result = removeDuplicates(nums);
+      expect(result).toBe(expected);
+      expect(nums.slice(0, result)).toEqual([]);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
